4e831046c9194625e1c4ced6121441fc0d3f86da
maramrmilad7 <maro12787@gmail.com>
2025-03-04 01:14:41 +0200

    create get all products services

--- lib/services/get_all_products_services.dart
+++ lib/services/get_all_products_services.dart
@@ -1,3 +1,23 @@
-class  allProductsServices{
-  
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import 'package:storeapplication/models/product_model.dart';
+
+class allProductsServices {
+   Future<List<ProductModel>> getAllProductsServices() async {
+    http.Response response =
+        await http.get(Uri.parse('https://fakestoreapi.com/products'));
+ if(response.statusCode == 200) {
+
+    List<dynamic> data = jsonDecode(response.body);
+    List<ProductModel> products = [];
+    for (var product in data) {
+      products.add(ProductModel.fromJson(product));
+    }
+    return products;    } 
+    else {
+      throw Exception('Failed to load products from the internet, there is a problem with status code ${response.statusCode}');
+    }
+  }
 }
