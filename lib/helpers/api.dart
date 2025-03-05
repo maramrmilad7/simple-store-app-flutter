1aedacc83cddf08cef811f55bad665ee706d13aa
maramrmilad7 <maro12787@gmail.com>
2025-03-06 00:30:08 +0200

    create put service

--- lib/helpers/api.dart
+++ lib/helpers/api.dart
@@ -37,4 +37,26 @@ class ApiServices {
       }
     }
   }
+   Future<dynamic> put(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
+      headers.addAll({'Content-Type': 'application/x-www-form-urlencoded'});
+
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+
+      http.Response response =
+          await http.post(Uri.parse(url), body: body, headers: headers);
+      if (response.statusCode == 200) {
+        List<dynamic> data = jsonDecode(response.body);
+
+        return data;
+      } else {
+        throw Exception(
+            'Failed to load products from the internet, there is a problem with status code ${response.statusCode}, with body ${response.body}');
+      }
+    }
+  }
 }
