02590bb624f100c20f8cf732fc14758829df4d50
maramrmilad7 <maro12787@gmail.com>
2025-03-06 00:02:37 +0200

    add post method

--- lib/helpers/api.dart
+++ lib/helpers/api.dart
@@ -1,23 +1,40 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class ApiServices{
+class ApiServices {
+  Future<dynamic> get({required String url}) async {
+    http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
 
+      return data;
+    } else {
+      throw Exception(
+          'Failed to load products from the internet, there is a problem with status code ${response.statusCode}');
+    }
+  }
 
+  Future<dynamic> post(
+      {required String url,
+      @required dynamic body,
+      @required String? token}) async {
+    Map<String, String> headers = {};
 
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
 
-   Future<dynamic> get({required String url}) async {
-    http.Response response =
-        await http.get(Uri.parse(url));
- if(response.statusCode == 200) {
+      http.Response response =
+          await http.post(Uri.parse(url), body: body, headers: headers);
+      if (response.statusCode == 200) {
+        List<dynamic> data = jsonDecode(response.body);
 
-    List<dynamic> data = jsonDecode(response.body);
-   
-    return data;    } 
-    else {
-      throw Exception('Failed to load products from the internet, there is a problem with status code ${response.statusCode}');
+        return data;
+      } else {
+        throw Exception(
+            'Failed to load products from the internet, there is a problem with status code ${response.statusCode}');
+      }
     }
   }
 }
-
